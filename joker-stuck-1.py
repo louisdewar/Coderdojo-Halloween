2b82b9528bb0da4bdf6ef9c9e4ec0a1a57ef0f80
louisdewar <louis@dewardt.co.uk>
2016-10-25 11:06:49 +0100

    Add files via upload

---
+++ joker-stuck-1.py
@@ -0,0 +1,13 @@
+# Define the jokes varialbe with two jokes in it.
+jokes = ['''
+Why didn't the mummy have any friends?
+(Because he was wrapped up in himself!)
+''','''
+What road has the most ghosts haunting it?
+(A dead end)
+''']
+
+# Loop through the jokes making the variable joke become each element of the array
+for joke in jokes:
+    # Print the joke
+    print(joke)
